9f024ead155d1eae5fb6cc1bf25e8435159e1aaa
Billy Wellington <billierangwani@gmail.com>
2024-08-12 23:59:00 +0200

    ℹ️ Add U.S. States Game using Python Turtle Graphics
    🗺️ Developed a U.S. States game using Python's turtle graphics. The game challenges players to name all 50 U.S. states by clicking on their locations on a map. Key features include:
    - Interactive map where correct guesses are displayed
    - Tracking and displaying of correct and missed states
    - Utilized data handling techniques to manage state names and positions
    - Incorporated user-friendly feedback and scoring system
    
    Enhanced educational value by providing a fun and engaging way to learn U.S. geography.
    
    #python #turtlegraphics #usstates #educationalgame

--- 100_Days_Code_Challenge/Day_26/main.py
+++ 100_Days_Code_Challenge/Day_26/main.py
@@ -1,7 +1,11 @@
-numbers = [1, 2, 3]
-new_list = [n + 1 for n in numbers]
+# numbers = [1, 2, 3]
+# new_list = [n + 1 for n in numbers]
+# # print(new_list)
+#
+# new_list = [n * 2 for n in range(1, 5)]
 # print(new_list)
+#
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
 
-new_list = [n * 2 for n in range(1, 5)]
-print(new_list)
-
+new_names = [name.upper() for name in names if (len(name) > 5)]
+print(new_names)
