94ae1f7cf434cc0e70999deb1c320c036264f033
Billy Wellington <billierangwani@gmail.com>
2024-08-15 23:57:47 +0200

    ℹ️ Add NATO Phonetic Alphabet Converter using List and Dict Comprehension
    🔤 Developed a Python program that converts a user-inputted word into a list of corresponding NATO phonetic alphabet codes. Key learnings and features include:
    - Utilized dictionary comprehension to map letters to their phonetic alphabet equivalents
    - Employed list comprehension to efficiently convert user input into a list of phonetic codes
    
    Improved code readability and efficiency through the use of comprehension techniques.
    
    #python #natoalphabet #listcomprehension #dictcomprehension #dataprocessing

--- 100_Days_Code_Challenge/Day_26/NATO-alphabet-start/main.py
+++ 100_Days_Code_Challenge/Day_26/NATO-alphabet-start/main.py
@@ -20,7 +20,7 @@ for (index, row) in student_data_frame.iterrows():
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
+#Create a dictionary in this format:
 with open("nato.csv") as file:
     list = [line.strip() for line in file.readlines()]
 
@@ -29,7 +29,7 @@ list.pop(0)
 nato_dict = {row.split(",")[0]: row.split(",")[1] for row in list}
 # print(nato_dict)
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+#TCreate a list of the phonetic code words from a word that the user inputs.
 word = input("Enter a word: ").upper()
 list = [nato_dict[letter] for letter in word]
 
